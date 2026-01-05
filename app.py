b3940a761f38378b96ec58e28cf7f8c63e4d12d3
Pulya063 <w71439@student.wsiz.edu.pl>
2026-01-05 01:23:10 +0100

    All have been taken to sqlalchemy

--- app.py
+++ app.py
@@ -8,25 +8,6 @@ import database
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
-def film_dictionary(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-
-class Connection_db:
-    def __init__(self):
-        self.conn = sqlite3.connect('database.db')
-        self.conn.row_factory = film_dictionary
-        self.cur = self.conn.cursor()
-
-    def __enter__(self):
-        return self.cur
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.conn.commit()
-        self.conn.close()
-
 def login_check(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -39,7 +20,7 @@ def login_check(func):
 @login_check
 def main_page():
     database.get_db()
-    films = select(Film).order_by(Film.id.limit(10).desc)
+    films = select(Film).order_by(Film.id.desc).limit(10)
     result_films = database.db_session.execute(films).scalars()
 
     return render_template('main_page.html', films=result_films)
@@ -126,23 +107,22 @@ def user_profile(user_id):
 
         action = request.form.get('action')
         if action == "save":
-            update_user = update(User).where(id = user_id).values(first_name=first_name, last_name=last_name, login=login, password=password, email=email, phone=phone, birth_date=birth_date, additional_info=additional_info)
+            update_user = update(User).where(User.id == user_id).values(first_name=first_name, last_name=last_name, login=login, password=password, email=email, phone=phone, birth_date=birth_date, additional_info=additional_info)
             res_upd_user = database.db_session.execute(update_user).scalar()
             if res_upd_user is None:
                 return render_template('message.html', details="User haven`t been updated", type="error")
             return render_template('message.html', details="User updated successfully", type="success")
 
         elif action == "delete":
-            cur.execute("DELETE FROM user WHERE id=?", (user_id,))
+            deleted_user = delete(User).where(User.id == user_id)
             session.clear()
             return render_template('message.html',
                                    details="Account deleted",
                                    type="success")
     else:
-
-        with Connection_db() as cur:
-            user_info = cur.execute(f'SELECT * FROM user WHERE id = ?', (user_id,)).fetchone()
-        return render_template('user_profile.html', user=user_info, session_user=session_user_id)
+        user_info = select(User).where(User.id == user_id)
+        res_user_info = database.db_session.execute(user_info).scalar()
+        return render_template('user_profile.html', user=user_info, session_user=res_user_info)
 
 
 
@@ -151,54 +131,45 @@ def user_profile(user_id):
 def user_delete(user_id):
     return f'User {user_id} delete!'
 
+
 @app.route('/films', methods=['GET'])
 def films():
-    filter_params = request.args
-    query_parts = []
-    params = []
-    join = ""
+    database.get_db()
 
-    with Connection_db() as cur:
-        countries = cur.execute(f'select * from country').fetchall()
-        genres = cur.execute(f'select * from genre').fetchall()
+    stmt = select(Film).distinct()
 
-    for key, value in filter_params.items():
-        if value:
-            if key == 'name':
-                query_parts.append("name LIKE ?")
-                params.append(f"%{value.strip()}%")
-            elif key == 'genre':
-                query_parts.append("g.genre LIKE ?")
-                join = "f inner join genre_film gn on f.id = gn.film_id inner join genre g on gn.genre_id = g.genre"
-                params.append(f"%{value.strip()}%")
-            elif key == 'first_name':
-                query_parts.append("a.first_name LIKE ?")
-                join = "f inner join actor_film af on f.id == af.film_id inner join actor a on af.actor_id == a.id"
-                params.append(f"%{value.strip()}%")
-            elif key == 'last_name':
-                query_parts.append("a.last_name LIKE ?")
-                join = "f inner join actor_film af on f.id == af.film_id inner join actor a on af.actor_id == a.id"
-                params.append(f"%{value.strip()}%")
-            else:
-                query_parts.append(f"{key} = ?")
-                params.append(value.strip())
+    countries = database.db_session.execute(select(Country)).scalars().all()
+    genres_list = database.db_session.execute(select(Genre)).scalars().all()
 
-    base_sql = "select * from film"
+    name = request.args.get('name')
+    genre_val = request.args.get('genre')
+    first_name = request.args.get('first_name')
+    last_name = request.args.get('last_name')
+    country_id = request.args.get('country')
 
-    if query_parts:
-        full_sql = f"{base_sql} {join} where {' and '.join(query_parts)} order by id desc"
-    else:
-        full_sql = f"{base_sql} order by id desc"
+    if name:
+        stmt = stmt.where(Film.name.icontains(name.strip()))
 
-    with Connection_db() as cur:
-        result = cur.execute(full_sql, params).fetchall()
+    if genre_val:
+        stmt = stmt.join(Film.genres).where(Genre.genre == genre_val)
+
+    if first_name or last_name:
+        stmt = stmt.join(Film.actors)
+        if first_name:
+            stmt = stmt.where(Actor.first_name.icontains(first_name.strip()))
+        if last_name:
+            stmt = stmt.where(Actor.last_name.icontains(last_name.strip()))
+
+    if country_id:
+        stmt = stmt.where(Film.country_id == country_id)
+
+    stmt = stmt.order_by(Film.id.desc())
+    result = database.db_session.execute(stmt).scalars().all()
 
     if not result:
-        flash("Film not found", "error")
-        return redirect(url_for('films'))
+        flash("Any film doesn`t been found", "error")
 
-    return render_template('film.html', films=result, countries=countries, genres=genres)
-
+    return render_template('film.html', films=result, countries=countries, genres=genres_list)
 
 
 @app.route('/films/<int:film_id>', methods=['PUT', 'GET'])
@@ -206,9 +177,9 @@ def film_edit(film_id):
     if film_id is None:
         return f'Film {film_id} does not exist!'
     if request.method == 'GET':
-        with Connection_db() as cur:
-            res = cur.execute(f'SELECT * FROM film WHERE id = ?', (film_id,)).fetchone()
-        return f'Film {film_id} it is {res}!'
+        film = select(Film).where(Film.id == film_id)
+        film = database.db_session.execute(film).scalar()
+        return f'Film {film_id} it is {film}!'
     return f'The received method is PUT'
 
 @app.route('/films/<int:film_id>', methods=['POST'])
@@ -221,22 +192,22 @@ def film_delete(film_id):
 
 @app.route('/films/<int:film_id>/rating', methods=['GET'])
 def show_film_ratings(film_id):
-    with Connection_db() as cur:
-        res = cur.execute(f'SELECT rating FROM film WHERE id = ?', (film_id,)).fetchone()
-    return f'Film {film_id} rating is {res}!'
+    film_rating = select(Film, Film.rating).where(Film.id == film_id)
+    film_rating = database.db_session.execute(film_rating).all()
+    return f'Film {film_id} rating is {film_rating}!'
 
 @app.route('/films/<int:film_id>/rating', methods=['POST'])
 def create_film_rating(film_id):
     return f'Film {film_id} rating!'
 
 @app.route('/films/<int:film_id>/ratings/<int:feedback_id>', methods=['GET', 'PUT'])
-def show_film_rating(film_id, feedback_id):
+def show_film_feedback(feedback_id):
     if request.method == 'GET':
         if feedback_id is None:
             return f'Feedback {feedback_id} does not exist!'
-        with Connection_db() as cur:
-            res = cur.execute(f'SELECT * FROM feedback WHERE id = ?', (feedback_id,)).fetchone()
-        return f'Feedback {feedback_id} it is {res}!'
+        film_feedback = select(Feedback).where(Feedback.id == feedback_id)
+        film_feedback = database.db_session.execute(film_feedback).scalar()
+        return f'Feedback {feedback_id} it is {film_feedback}!'
     return f'The received method is PUT'
 
 @app.route('/films/<int:film_id>/ratings/<int:feedback_id>', methods=['DELETE'])
@@ -247,9 +218,9 @@ def film_ratings_delete(film_id, feedback_id):
 @login_check
 def user_lists(user_id):
     if request.method == 'GET':
-        with Connection_db() as cur:
-            res = cur.execute(f'SELECT * FROM list WHERE id = ?', (user_id,)).fetchall()
-        return f'List {user_id} are {res}'
+        film_lists = select(MovieList).where(MovieList.user_id == user_id)
+        film_lists = database.db_session.execute(film_lists).all()
+        return f'List {user_id} are {film_lists}'
     return f'The received method is POST'
 
 @app.route('/users/<int:user_id>/lists/<int:list_id>', methods=['DELETE'])
@@ -260,9 +231,9 @@ def list_delete(user_id, list_id):
 @app.route('/users/<int:user_id>/lists/<int:list_id>', methods=['GET'])
 @login_check
 def list_show(user_id, list_id):
-    with Connection_db() as cur:
-        res = cur.execute(f'SELECT name, genre FROM film join film_list on film.id = film_list.film_id WHERE film_list.list_id= ?', (list_id,)).fetchall()
-    return f'User {user_id} list item {list_id} have such films {",".join(i for i in res)}'
+    film_list = select(Film, Film.name, Film.genre).join(FilmList, Film.id == FilmList.film_id).where(FilmList.list_id == list_id)
+    film_list = database.db_session.execute(film_list).scalar()
+    return f'User {user_id} list item {list_id} have such films {",".join(i for i in film_list)}'
 
 @app.route('/users/<int:user_id>/lists/<int:list_id>', methods=['POST'])
 @login_check
